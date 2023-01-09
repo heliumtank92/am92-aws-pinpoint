da3467b0af085a1b50eed709a28a2e693dfd4753
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-01-09 12:18:04 +0530

    Revamp. Add Errors

---
+++ src/ERRORS.mjs
@@ -0,0 +1,19 @@
+export const SEND_MESSAGE_ERROR = {
+  errorCode: 'AwsPinpointError::SEND_MESSAGE'
+}
+
+export const SEND_EMAIL_ERROR = {
+  errorCode: 'AwsPinpointError::SEND_EMAIL'
+}
+
+export const SEND_SMS_ERROR = {
+  errorCode: 'AwsPinpointError::SEND_SMS'
+}
+
+export const GET_EMAIL_TEMPLATE_ERROR = {
+  errorCode: 'AwsPinpointError::GET_EMAIL_TEMPLATE'
+}
+
+export const GET_SMS_TEMPLATE_ERROR = {
+  errorCode: 'AwsPinpointError::GET_SMS_TEMPLATE'
+}
